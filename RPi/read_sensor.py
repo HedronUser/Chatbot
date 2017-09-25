a2cc5914224826514b3500c5be40bdda2ddacc86
Jesse Jenkins <jesse@hedron.technology>
2017-09-25 23:20:44 +0000

    all 4 directions are parsing over serial well. Now need to test how fast we can go over serial (lower the 1 second delay)

--- RPi/read_sensor.py
+++ RPi/read_sensor.py
@@ -25,14 +25,30 @@ while True:
     x = ser.readline()
     words = x.split()
     
+
+    
 ##    print words[0]
 ##    print words[2]
+##    print words[3]
+##    print words[5]
     
+##    
     if (words[0] == 'front' and words[2] == '1'):
         print 'Obstacle in front'
     elif (words[0] == 'front' and words[2] == '0'):
         print 'Obstacle free'
-        
+    if (words[0] == 'right' and words[2] == '1'):
+        print 'Obstacle on right'
+    elif (words[0] == 'right' and words[2] == '0'):
+        print 'Obstacle free'
+    if (words[0] == 'rear' and words[2] == '1'):
+        print 'Obstacle in rear'
+    elif (words[0] == 'rear' and words[2] == '0'):
+        print 'Obstacle free'
+    if (words[0] == 'left' and words[2] == '1'):
+        print 'Obstacle on left'
+    elif (words[0] == 'left' and words[2] == '0'):
+        print 'Obstacle free'        
 
 
 
