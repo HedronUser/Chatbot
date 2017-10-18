cf29f6f9f99ab0bab3e9b2eb6ab482d1ad317571
Jesse Jenkins <jessetjenkins@gmail.com>
2017-10-18 10:35:55 -0700

    This test bit is meant to receive Json from the RPi.

--- Teensy_Controller/Json_read/Json_read/Json_read.ino
+++ Teensy_Controller/Json_read/Json_read/Json_read.ino
@@ -1,45 +1,92 @@
 #include <ArduinoJson.h>
+#include <SoftwareSerial.h> //this is needed for alternate serial pin designations on Teensy 3
+#define TX3_PIN 8
+#define RX3_PIN 7
+SoftwareSerial  SerialPort2(RX3_PIN, TX3_PIN);
+
+int driveval = 0;
+int turnval = 0;
+int strafeval = 0;
+
+const char startMarker = '<';
+const char endMarker = '>';
 
 void setup() {
   // put your setup code here, to run once:
- 
-  
-  // JSON input string.
-  //
-  // It's better to use a char[] as shown here.
-  // If you use a const char* or a String, ArduinoJson will
-  // have to make a copy of the input in the JsonBuffer.
-
-  // Root of the object tree.
-  //
-  // It's a reference to the JsonObject, the actual bytes are inside the
-  // JsonBuffer with all the other nodes of the object tree.
-  // Memory is freed when jsonBuffer goes out of scope.
+  pinMode(13, OUTPUT); // the onboard LED
+  SerialPort2.begin(57600);
   Serial.begin(9600);
+  delay(10000);
 
-  }
+  debugToPC("Arduino Ready from ArduinoPC.ino");
+  //Serial.print("sent ready message");
+  delay(500);  //Serial.print("sent ready message");
+}
 
-//  char json[] = "{\"drive\":0,\"strafe\":0,\"turn\":0}";
 
+unsigned long curMillis;
+unsigned long prevCheckWifi = 0;
+unsigned long checkWifiInterval = 50; //checks wifi for new data every 100 ms
 
 
 void loop() {
-    StaticJsonBuffer<200> jsonBuffer;
+  curMillis = millis();
+  
+//  if(((curMillis - prevCheckWifi) > checkWifiInterval)){
+    readWifi();
+//    Serial.println();
+//    Serial.print("Updating Wifi");
+//    Serial.println();
+    prevCheckWifi = curMillis;
+//  }
+ 
+}
+
+bool readWifi(void){
+if (SerialPort2.available() > 2){
+   StaticJsonBuffer<200> jsonBuffer;
 
   // put your main code here, to run repeatedly:
-   JsonObject& root = jsonBuffer.parseObject(Serial);
-  // Test if parsing succeeds.
+   JsonObject& root = jsonBuffer.parseObject(SerialPort2);
+
+    // Test if parsing succeeds.
   if (!root.success()) {
     Serial.println("parseObject() failed");
-    return;
+    return false;
   }
-     int driveval = root["drive"];
-     int turnval = root["turn"];
-     int strafeval = root["strafe"];
-//
+ driveval = root["drive"];
+ turnval = root["turn"];
+ strafeval = root["strafe"];
+
  Serial.print("driveVal: ");
  Serial.print(driveval);
  Serial.println();
+ 
+ Serial.print("strafeVal: ");
+ Serial.print(strafeval);
+ Serial.println();
+ 
+ Serial.print("turnVal: ");
+ Serial.print(turnval);
+ Serial.println();
 // Serial.println();
-
+return true;
 }
+//Serial.println("Serial not available");
+}
+
+void debugToPC( byte num) {
+    byte nb = 0;
+    SerialPort2.write(startMarker);
+    SerialPort2.write(nb);
+    SerialPort2.print(num);
+    SerialPort2.write(endMarker);
+}
+void debugToPC( char arr[]) {
+    byte nb = 0;
+    SerialPort2.write(startMarker);
+    SerialPort2.write(nb);
+    SerialPort2.print(arr);
+    SerialPort2.write(endMarker);
+}
+
